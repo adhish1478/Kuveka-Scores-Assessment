5414c01fd17f8ee8257957b7a067489832e530a6
adhish1478 <adhish.aravind7@gmail.com>
2025-09-17 19:30:56 +0530

    upload offer api and routine done, utils file created

--- scores/urls.py
+++ scores/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import UploadOffer
 
 urlpatterns=[
-    
+    path('offer/', UploadOffer.as_view(), name='upload_offer'),
 ]
--- scores/views.py
+++ scores/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . import utils
 # Create your views here.
+OFFER = {}
+LEADS = []
+SCORED_RESULTS = []
+
+class UploadOffer(APIView):
+    def post(self, request):
+        global OFFER
+        OFFER.clear()
+        OFFER.update(request.data)
+        return Response(
+            {
+                "message": "Offer uploaded successfully",
+                "offer": OFFER
+            },
+            status=status.HTTP_201_CREATED
+)
